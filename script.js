7d045b13cbe6558497df5501ad300af8542564cb
Anderson Alencar <anderson23alencar@gmail.com>
2021-08-25 16:03:07 -0300

    correções 2.0 linter

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const btnLogin = document.getElementById('button-login');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 
-btnLogin.addEventListener('click', login);
-
-function login () {
+function login() {
   if (emailInput.value === 'tryber@teste.com' && passwordInput.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
 }
+
+btnLogin.addEventListener('click', login);
